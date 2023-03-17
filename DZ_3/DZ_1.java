2a70d0b5101bda92c35d5a393e32ef62ba865482
YaroSLAVE <llapbkob@vk.com>
2023-03-17 20:14:43 +0400

    done task 1

--- DZ_3/DZ_1.java
+++ DZ_3/DZ_1.java
@@ -1,8 +1,25 @@
 package DZ_3;
-
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Random;
 public class DZ_1 {
     public static void main(String[] args) {
-            
+        ArrayList<Integer> myArray = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < random.nextInt(30); i++) {
+            myArray.add(random.nextInt(10));
+        }
+
+        System.out.println(myArray);
+
+        Iterator<Integer> myArrayIterator = myArray.iterator();
+        while (myArrayIterator.hasNext()){
+            int myArrayNext = myArrayIterator.next();
+            if (myArrayNext % 2 == 0){
+                myArrayIterator.remove();
+            }
+        }
+        System.out.println(myArray);  
 }
     
 }
